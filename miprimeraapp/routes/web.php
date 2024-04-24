59d454e960becf6d54c057cc6b5fee6160e9b6c9
Eduardo Mora <eduardo@toolsnet.mx>
2024-04-24 12:26:16 -0600

    first commit

---
+++ miprimeraapp/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return '<h1>Homepage</h1><a href="/about">View the about page</a>';
+});
+
+Route::get('/about',function(){
+    return '<h1>About Page</h1><a href="/">Back to home<a>';
+});
