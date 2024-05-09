dae7db19b8b770c29df6782c35b156aba5e11437
Eduardo Mora <eduardo@toolsnet.mx>
2024-05-09 13:25:43 -0600

    se agregó función para hacer post en la app

--- firstapp/app/Http/Controllers/PostController.php
+++ firstapp/app/Http/Controllers/PostController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    // mostrar nueva publicación
+    public function storeNewPost(Request $request){
+        $entradas = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $entradas['title'] = strip_tags($entradas['title']);
+        $entradas['body'] = strip_tags($entradas['body']);
+        $entradas['user_id'] = auth()->id();
+
+        Post::create($entradas);
+
+        return 'post created';
+    }
+
     // Función hacer una publicación
     public function showCreateForm(){
-        return 'hello !!';
+
+        return view('create-post');
     }
+
 }
--- firstapp/routes/web.php
+++ firstapp/routes/web.php
@@ -25,4 +25,5 @@ Route::post('/login',[UserController::class, "login"]);
 Route::post('/logout',[UserController::class, "logout"]);
 
 // Blog post related routes
-Route::get('/create-post',[PostController::class,'showCreateForm']);
+Route::get('/create-post',[PostController::class, 'showCreateForm']);
+Route::post('/create-post',[PostController::class, 'storeNewPost']);
